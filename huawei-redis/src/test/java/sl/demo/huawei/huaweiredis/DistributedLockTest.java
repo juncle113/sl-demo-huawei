1b1c3d5dc1ecfee66fbf54b562671ce017181b37
juncle113 <sunli20140406@foxmail.com>
2020-01-22 16:03:15 +0800

    修改售卖条件

--- huawei-redis/src/test/java/sl/demo/huawei/huaweiredis/DistributedLockTest.java
+++ huawei-redis/src/test/java/sl/demo/huawei/huaweiredis/DistributedLockTest.java
@@ -15,15 +15,16 @@ public class DistributedLockTest {
 class ServiceOrder {
     private final int MAX = 10;
     DistributedLock DLock = new DistributedLock();
-    int n = 10;
+    int n = 0;
 
     public void handleOder() {
         String userName = UUID.randomUUID().toString().substring(0, 8) + Thread.currentThread().getName();
         String identifier = DLock.getLockWithTimeout("Mate 10", 10000, 2000);
         System.out.println("正在为用户：" + userName + " 处理订单");
-        if (n > 0) {
-            int num = MAX - n + 1;
-            System.out.println("用户：" + userName + "购买第" + num + "台，剩余" + (--n) + "台");
+        if (n < MAX) {
+            n++;
+            System.out.println("用户：" + userName + "购买第" + n + "台，剩余" + (MAX - n) + "台");
+
         } else {
             System.out.println("用户：" + userName + "无法购买，已售罄！");
         }
